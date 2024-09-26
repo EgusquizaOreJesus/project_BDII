23c81366d6716b10ea35ef0da3fd7ad3191d3fc0
Sebastian Chu <45053945+ChuSebastian@users.noreply.github.com>
2024-09-25 21:00:55 -0500

    Bug en rebuild corregido.
    Pendiente:
    - Agregar remove.

--- Sequential.cpp
+++ Sequential.cpp
@@ -141,8 +141,7 @@ public:
 
     // Se reconstruye apenas llega al limite de elementos
     if (isize == floor(log2(msize)))
-      cout << "rebuild" << endl;
-    // rebuild();
+      rebuild();
   }
 
   void rebuild()
@@ -150,12 +149,20 @@ public:
     string new_file = "new.bin";
     ifstream ifile(insert_file, ios::binary);
     ifstream mfile(main_file, ios::binary);
+
+    ifstream check(new_file, ios::binary);
+    if (!check)
+    {
+      ofstream create(new_file, ios::binary);
+      create.close();
+    }
+
     fstream nfile(new_file, ios::in | ios::out | ios::binary);
 
     // Traer registros del archivo de los nuevos inserts a RAM
     int isize;
     ifile.read((char *)&isize, sizeof(int));
-
+  
     vector<Record> new_records;
     for (int i = 0; i < isize; i++)
     {
@@ -164,6 +171,7 @@ public:
       new_records.push_back(record);
     }
 
+    // Ordenar registros en RAM
     sort(new_records.begin(), new_records.end(), compare_key);
 
     // Escribir size del nuevo archivo
@@ -226,10 +234,16 @@ public:
       }
     }
 
+    // Actualizar la cantidad de registros en el nuevo archivo
+    nfile.seekp(0, ios::beg);
+    nfile.write((char*)&nsize, sizeof(int));
+
+    // Cerrar los archivos
     ifile.close();
     mfile.close();
     nfile.close();
 
+    // Eliminar archivos extra y solo nos quedamos con el nuevo archivo. 
     remove((char *)&main_file);
     remove((char *)&insert_file);
     rename((char *)&new_file, (char *)&main_file);
@@ -315,16 +329,16 @@ void rebuild_test()
   Sequential file("main.bin");
   file.buildCSV("test.csv");
   file.insert(150);
-  file.insert(550);
   file.insert(950);
+  file.insert(550);
   file.print_all();
 }
 
 int main()
 {
-  // build_test();
-  // show_all_test();
-  // search_test();
+  build_test();
+  show_all_test();
+  search_test();
   rebuild_test();
 
   return 0;
